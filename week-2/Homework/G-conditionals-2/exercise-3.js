31dd5e8e6206dcf5ecf1519234efbd2a4c3f0132
Jaime Bravo <jaimemagic1982@gmail.com>
2020-08-25 17:59:41 +0200

    homework week1

--- week-2/Homework/G-conditionals-2/exercise-3.js
+++ week-2/Homework/G-conditionals-2/exercise-3.js
@@ -9,9 +9,17 @@
 */
 
 function calculateGrade(mark) {
-
+if (mark >= 80) {
+return "A";
 }
-
+if (mark < 80 && mark > 60) {
+  return "B";
+}
+if (mark <= 60 && mark > 50) {
+  return "C";
+}
+else {return "F"}
+}
 /* 
 DO NOT EDIT BELOW THIS LINE
 --------------------------- */
